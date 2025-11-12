58ee864567a93a3b656a15f2dabb4bfc0513190c
Deividgaston <157326724+Deividgaston@users.noreply.github.com>
2025-11-12 23:29:29 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBTK8altmAR-fWqR9BjE74gEGavuiqk1Bs",
-  authDomain: "gastos-2n.firebaseapp.com",
-  projectId: "gastos-2n",
-  storageBucket: "gastos-2n.firebasestorage.app",
-  messagingSenderId: "55010048795",
-  appId: "1:55010048795:web:4fb48d1e0f9006ebf7b1be"
-};
-window.__FBCONFIG__ = firebaseConfig;
